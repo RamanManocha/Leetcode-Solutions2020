cb38baa98e082157b0d125cc68100c3b9bbde52f
ramankgp <39129293+ramankgp@users.noreply.github.com>
2020-08-10 19:34:49 +0530

    Update palindrome-partitioning.cpp

--- Backtracking/palindrome-partitioning.cpp
+++ Backtracking/palindrome-partitioning.cpp
@@ -15,7 +15,13 @@ Output:
 ]
 
 
+The problem has a nice structure that backtracking naturally fits in. The structure is, given a starting position start, we search from start till the end of the string. Once we reach a position i such that the sub-string from start to i (s.substr(start, i - start + 1)) is a palindrome, we add it to a temporary par. Then we recursively call the same function to process the remaining sub-string. Once we reach the end of the string, we add par into the result pars 
+of all the possible partitioning.
 
+Then, backtracking happens. Remember that at position i, we find s.substr(start, i - start + 1) to be a palindrome and we 
+immediately add it to par. It is obvious that there may be some other position j such that j > i and s.substr(start, j - start + 1) 
+  is also a palindrome. So we need to recover to the state before adding s.substr(start, i - start + 1) to par and continue to find
+  the next palindrome position after i. And we simply need to pop s.substr(start, i - start + 1) out of par to make things work.
 
 class Solution {
 public:
