291fca06c1b997c9da2360c83ec949096f287018
ramankgp <39129293+ramankgp@users.noreply.github.com>
2020-08-21 13:55:00 +0530

    Create Count Inversions in an array.cpp

---
+++ Sorting/MergeSort/Count Inversions in an array.cpp
@@ -0,0 +1,53 @@
+
+https://www.geeksforgeeks.org/counting-inversions/
+
+
+Inversion Count for an array indicates – how far (or close) the array is from being sorted. If array is already sorted then inversion count is 0. If array is sorted in reverse order that inversion count is the maximum.
+Formally speaking, two elements a[i] and a[j] form an inversion if a[i] > a[j] and i < j
+
+Example:
+
+Input: arr[] = {8, 4, 2, 1}
+Output: 6
+
+Explanation: Given array has six inversions:
+(8,4), (4,2),(8,2), (8,1), (4,1), (2,1).
+
+
+Input: arr[] = {3, 1, 2}
+Output: 2
+
+Explanation: Given array has two inversions:
+(3, 1), (3, 2) 
+
+
+
+
+// C++ program to Count Inversions 
+// in an array 
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int getInvCount(int arr[], int n) 
+{ 
+	int inv_count = 0; 
+	for (int i = 0; i < n - 1; i++) 
+		for (int j = i + 1; j < n; j++) 
+			if (arr[i] > arr[j]) 
+				inv_count++; 
+
+	return inv_count; 
+} 
+
+// Driver Code 
+int main() 
+{ 
+	int arr[] = { 1, 20, 6, 4, 5 }; 
+	int n = sizeof(arr) / sizeof(arr[0]); 
+	cout << " Number of inversions are "
+		<< getInvCount(arr, n); 
+	return 0; 
+} 
+
+// This code is contributed 
+// by Akanksha Rai 
