b414485611311ef71177f6ad28aa6538cac4a3e4
ramankgp <39129293+ramankgp@users.noreply.github.com>
2020-08-22 15:57:20 +0530

    Update 261. Graph Valid Tree.cpp

--- Graphs/261. Graph Valid Tree.cpp
+++ Graphs/261. Graph Valid Tree.cpp
@@ -55,3 +55,37 @@ public:
          return edges.size()==n-1;
     }
 };
+
+
+
+
+
+Approach 2: using dfs
+
+
+class Solution {
+public:
+    bool validTree(int n, vector<pair<int, int>>& edges) {
+    vector<vector<int>> edgelists(n);
+    for(auto i:edges)
+    {
+        edgelists[i.first].push_back(i.second);
+        edgelists[i.second].push_back(i.first);
+    }
+    vector<bool> visited(n,false);
+    if(hascycle(edgelists,visited,0,-1)) return false;
+    for(auto i:visited)
+        if(!i) return false;
+    return true;
+    }
+    
+    bool hascycle(vector<vector<int>> &edgelists,vector<bool> &visited,int cur,int parent)
+    {
+        if(visited[cur]) return true;
+        visited[cur]=true;
+        for(auto kid:edgelists[cur])
+            if(kid!=parent&&hascycle(edgelists,visited,kid,cur))
+                return true;
+        return false;
+    }
+};
