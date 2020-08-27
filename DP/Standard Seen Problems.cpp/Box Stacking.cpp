2ddb15f2708d2aadf49322a90a4aa04afd7f3055
ramankgp <39129293+ramankgp@users.noreply.github.com>
2020-08-27 14:23:38 +0530

    Update Box Stacking.cpp

--- DP/Standard Seen Problems.cpp/Box Stacking.cpp
+++ DP/Standard Seen Problems.cpp/Box Stacking.cpp
@@ -5,6 +5,23 @@ higher box. Of course, you can rotate a box so that any side functions as its ba
 instances of the same type of box. You task is to complete the function maxHeight which returns the height of the highest possible stack so formed.
 
 
+	
+1) Generate all 3 rotations of all boxes. The size of rotation array becomes 3 times the size of the original array. For simplicity, we consider width as always smaller than or equal to depth.
+
+2) Sort the above generated 3n boxes in decreasing order of base area.
+
+3) After sorting the boxes, the problem is same as LIS with following optimal substructure property.
+MSH(i) = Maximum possible Stack Height with box i at top of stack
+MSH(i) = { Max ( MSH(j) ) + height(i) } where j < i and width(j) > width(i) and depth(j) > depth(i).
+If there is no such j then MSH(i) = height(i)
+
+4) To get overall maximum height, we return max(MSH(i)) where 0 < i < n
+
+Following is the implementation of the above solution.
+	
+	
+	
+	
 Input:
 n = 4
 height[] = {4,1,4,10}
