c4e97bb18528b55537d55826d98bcb16efe9c0c0
ramankgp <39129293+ramankgp@users.noreply.github.com>
2020-10-23 11:09:55 +0530

    Update Wildcard Matching.cpp

--- DP/Standard Seen Problems.cpp/Wildcard Matching.cpp
+++ DP/Standard Seen Problems.cpp/Wildcard Matching.cpp
@@ -2,6 +2,28 @@
 44. Wildcard Matching
 
 Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*'.
+	
+	
+https://procoderforu.com/wildcard-pattern-matching/
+
+
+We encounter a ‘?’ in the pattern. Since ‘?’ (P[i]) can be matched with only a single character (S[j]) of text , P[1…i] will match with S[1…j] if and only if P[1…i-1] matches with S[1…j-1]. In the language of algorithms, we can say:
+match(i, j) = match(i-1, j-1) if P[i] == '?'
+We do not encounter any special character in the pattern. In that case, P[1…i] will match with S[1…j] if and only if P[1…i-1] matches with S[1…j-1] and P[i] matches with S[j]. Formally, we can write it as:
+match(i, j) = match(i-1, j-1) && P[i]==S[j]  if P[i] not in {'?', '*'}
+We encounter a ‘*’ in the pattern. Now, there can further be 2 possibilities:
+We match empty sequence with ‘*’. In that case, P[1…i] will match with S[1…j] if and only if P[1…i-1] matches with S[1…j].
+We match ‘*’ with S[j]. In that case, P[1…i] will match with S[1…j] if and only if P[1…i] matches with S[1…j-1]. You can appreciate the fact that ‘*’ unlike ‘?’ can be used again for a continuous sequence.
+If any one of the above 2 cases returns true, then, match(i, j) will also return true. Formally speaking,
+
+match(i, j) = match(i-1, j) || match(i, j-1) if P[i] == '*'
+	
+	
+	
+	
+	
+	
+	
 
 '?' Matches any single character.
 '*' Matches any sequence of characters (including the empty sequence).
@@ -25,6 +47,56 @@ s = "aa"
 p = "*"
 Output: true
 Explanation: '*' matches any sequence.
+
+
+
+	
+	
+	
+	#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAX 1001
+
+string S, P;
+bool dp[MAX][MAX], computed[MAX][MAX];
+
+bool match(int i, int j)
+{
+    if (i==0 && j==0) return true;
+    
+    if (i==0) return false;
+
+    if (j==0 && P[i-1] != '*') return false;
+
+    if (computed[i][j]) return dp[i][j];
+    
+    computed[i][j] = true;
+
+    if (j==0 && P[i-1]=='*') return dp[i][j] = match(i-1, j);
+
+    if (P[i-1]=='?') return dp[i][j] = match(i-1, j-1);
+
+    if (P[i-1]!='?' && P[i-1]!='*')
+        return dp[i][j] = match(i-1, j-1) && P[i-1] == S[j-1];
+
+    return dp[i][j] = match(i-1, j) || match(i, j-1);
+}
+
+int main()
+{
+    memset(computed, false, sizeof(computed));
+    
+    S = "baaabab", P = "**a*****ab";
+
+    cout << match(P.length(), S.length()) << endl;
+    
+    // output is 1 (true)
+    
+}
+
+
 
 
 
