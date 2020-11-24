cef9cc0fe64476e150874b44b02c1e44fcd7cf8d
ramankgp <39129293+ramankgp@users.noreply.github.com>
2020-11-24 22:06:06 +0530

    Update Non-Adjacent Combination Sum.cpp

--- DP/Knapsack Variations/Non-Adjacent Combination Sum.cpp
+++ DP/Knapsack Variations/Non-Adjacent Combination Sum.cpp
@@ -1,6 +1,10 @@
 
 You are given a list of positive integers nums and an integer k. Return whether there exists a combination of integers in nums such that their sum is k and none of those elements are adjacent in the original list.
 
+        
+ https://binarysearch.com/room/Weekly-Contest-34-aDD9ZHbeO9?questionsetIndex=2
+
+
 Constraints
 
 n * k ≤ 100,000 where n is the length of nums
