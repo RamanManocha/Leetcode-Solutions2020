083d956bf14ab6f8a9d7872a5538fc5c8aaae389
ramankgp <39129293+ramankgp@users.noreply.github.com>
2020-11-28 11:38:53 +0530

    Update 210. Course Schedule II.cpp

--- Graphs/Topological Sort/210. Course Schedule II.cpp
+++ Graphs/Topological Sort/210. Course Schedule II.cpp
@@ -20,6 +20,48 @@ Explanation: There are a total of 2 courses to take. To take course 1 you should
 
 
 
+Approach 1:
+
+
+enum State { INIT, VISITING, VISITED };
+
+class Solution {
+ public:
+  vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+    vector<int> ans;
+    vector<vector<int>> graph(numCourses);
+    vector<State> state(numCourses);
+
+    for (const vector<int>& p : prerequisites)
+      graph[p[1]].push_back(p[0]);
+
+    for (int i = 0; i < numCourses; ++i)
+      if (hasCycle(i, graph, state, ans))
+        return {};
+
+    reverse(begin(ans), end(ans));
+    return ans;
+  }
+
+ private:
+  bool hasCycle(int i, const vector<vector<int>>& graph, vector<State>& state,
+                vector<int>& ans) {
+    if (state[i] == VISITING)
+      return true;
+    if (state[i] == VISITED)
+      return false;
+
+    state[i] = VISITING;
+    for (const int neighbor : graph[i])
+      if (hasCycle(neighbor, graph, state, ans))
+        return true;
+    state[i] = VISITED;
+    ans.push_back(i);
+
+    return false;
+  }
+};
+
 
 
 
