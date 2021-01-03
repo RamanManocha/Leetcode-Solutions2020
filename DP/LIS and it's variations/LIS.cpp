7e35518239c005ec51f332e163d6292c67928de0
ramankgp <39129293+ramankgp@users.noreply.github.com>
2021-01-03 19:52:36 +0530

    Update LIS.cpp

--- DP/LIS and it's variations/LIS.cpp
+++ DP/LIS and it's variations/LIS.cpp
@@ -1,3 +1,52 @@
+
+
+
+
+
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        if(n == 0){
+            return 0;
+        }
+        
+        vector<int> lis;
+        
+        lis.push_back(nums[0]);
+        
+        for(int i = 1; i<n; i++){
+            
+            if(nums[i] > lis.back()){
+                lis.push_back(nums[i]);
+            }
+            else{
+                
+                int p = lower_bound(lis.begin(),lis.end(),nums[i]) - lis.begin();
+                
+                lis[p] = nums[i];
+            }
+        }
+        
+        return lis.size();
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* Dynamic Programming C++ implementation of LIS problem */
 #include<bits/stdc++.h> 
 using namespace std; 
