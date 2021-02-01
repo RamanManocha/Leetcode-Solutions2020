a11108dc69b895e8c324e97ad6de7dc251d3b7df
ramankgp <39129293+ramankgp@users.noreply.github.com>
2021-02-01 17:36:57 +0530

    Update Collecting Disappearing Coins.cpp

--- DP/Kadane DP/Collecting Disappearing Coins.cpp
+++ DP/Kadane DP/Collecting Disappearing Coins.cpp
@@ -2,6 +2,11 @@
 Collecting Disappearing Coins
 You are given a two-dimensional list of integers matrix where each matrix[r][c] represents the number of coins in that cell. When you pick up coins on matrix[r][c], all the coins on row r - 1 and r + 1 disappear, as well as the coins at the two cells matrix[r][c + 1] and matrix[r][c - 1]. Return the maximum number of coins that you can collect.
 
+    
+    https://binarysearch.com/problems/Collecting-Disappearing-Coins
+
+
+
 Constraints
 
 n, m ≤ 250 where n and m are the number of rows and columns in matrix
