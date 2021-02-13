946f2cae6218688ff0ab8e8f6c298a1d21f724bb
ramankgp <39129293+ramankgp@users.noreply.github.com>
2021-02-13 12:36:54 +0530

    Update Diameter of Binary Tree.cpp

--- Trees/Diameter Problems/Diameter of Binary Tree.cpp
+++ Trees/Diameter Problems/Diameter of Binary Tree.cpp
@@ -20,7 +20,22 @@ Given a binary tree
 Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
 
 
+Height of a Binary Tree
+Let us see how to calculate the height of a binary tree using post order traversal.
 
+Suppose we know the height of the left and right subtree. What is the height of the root? Clearly, it is max(leftHeight,rightHeight) + 1.
+This gives us the algorithm. Recursively calculate the height of the subtrees and update the height of the root. All that is left is to handle the corner cases.
+If both the subtrees are non empty, we are done, as the formula is correct.
+Suppose, exactly one subtree is empty. Then the result would be nonEmptyTreeHeight + 1. This is also valid.
+What happens if both the subtree is empty? This means that the root is a leaf node. According to the formula, the height of the root is 1.
+Conclusion ===> We are assuming the height of a leaf node as 1.
+Calculating the diameter
+The final diameter has the following form. It goes up certain nodes and after a specific node, say head, it comes down.
+Recall that while calculating the height of the tree, the height was computed for each and every node due to recursion.
+As soon as the height of the node head is computed, we can get the diameter as leftHeight + rightHeight. Observe that we didn't account for the
+          fact that there are 2 extra edges to reach the left and right subtree. However, this is taken care of by the assumption that leaf nodes have height 1. 
+          There is 1 leaf node in each tree which compensates the lost edges. Moreover, this also gives us that the diameter of a leaf node is 0,
+          while its height is 1.
 
 
 
