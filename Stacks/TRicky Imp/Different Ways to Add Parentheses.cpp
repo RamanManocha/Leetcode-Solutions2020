47c840dc09c55109366cbdf1071a23ff5c283eba
ramankgp <39129293+ramankgp@users.noreply.github.com>
2021-04-20 22:07:59 +0530

    Update Different Ways to Add Parentheses.cpp

--- Stacks/TRicky Imp/Different Ways to Add Parentheses.cpp
+++ Stacks/TRicky Imp/Different Ways to Add Parentheses.cpp
@@ -2,11 +2,15 @@
 
 
 241. Different Ways to Add Parentheses
-Medium
 
-1928
 
-101
+
+
+
+https://leetcode.com/problems/different-ways-to-add-parentheses/discuss/66331/C%2B%2B-4ms-Recursive-and-DP-solution-with-brief-explanation
+
+
+
 
 Add to List
 
@@ -59,6 +63,40 @@ Explanation:
 
 
 
+  
+  
+  class Solution {
+public:
+    vector<int> diffWaysToCompute(string input) {
+        vector<int> result;
+        int size = input.size();
+        for (int i = 0; i < size; i++) {
+            char cur = input[i];
+            if (cur == '+' || cur == '-' || cur == '*') {
+                // Split input string into two parts and solve them recursively
+                vector<int> result1 = diffWaysToCompute(input.substr(0, i));
+                vector<int> result2 = diffWaysToCompute(input.substr(i+1));
+                for (auto n1 : result1) {
+                    for (auto n2 : result2) {
+                        if (cur == '+')
+                            result.push_back(n1 + n2);
+                        else if (cur == '-')
+                            result.push_back(n1 - n2);
+                        else
+                            result.push_back(n1 * n2);    
+                    }
+                }
+            }
+        }
+        // if the input string contains only number
+        if (result.empty())
+            result.push_back(atoi(input.c_str()));
+        return result;
+    }
+    
+    
+    
+    
 class Solution {
 private:
     unordered_map<string, vector<int>>memo;
@@ -104,3 +142,65 @@ public:
         return result;
     }
 };
+
+
+
+
+
+
+Optimised Approach:
+
+
+
+
+class Solution {
+public:
+	vector<int> diffWaysToCompute(string input) {
+		unordered_map<string, vector<int>> dpMap;
+		return computeWithDP(input, dpMap);
+	}
+
+	vector<int> computeWithDP(string input, unordered_map<string, vector<int>> &dpMap) {
+		vector<int> result;
+		int size = input.size();
+		for (int i = 0; i < size; i++) {
+			char cur = input[i];
+			if (cur == '+' || cur == '-' || cur == '*') {
+				// Split input string into two parts and solve them recursively
+				vector<int> result1, result2;
+				string substr = input.substr(0, i);
+				// check if dpMap has the result for substr
+				if (dpMap.find(substr) != dpMap.end())
+					result1 = dpMap[substr];
+				else
+					result1 = computeWithDP(substr, dpMap);
+
+				substr = input.substr(i + 1);
+				if (dpMap.find(substr) != dpMap.end())
+					result2 = dpMap[substr];
+				else
+					result2 = computeWithDP(substr, dpMap);
+				
+				for (auto n1 : result1) {
+					for (auto n2 : result2) {
+						if (cur == '+')
+							result.push_back(n1 + n2);
+						else if (cur == '-')
+							result.push_back(n1 - n2);
+						else
+							result.push_back(n1 * n2);
+					}
+				}
+			}
+		}
+		// if the input string contains only number
+		if (result.empty())
+			result.push_back(atoi(input.c_str()));
+		// save to dpMap
+		dpMap[input] = result;
+		return result;
+	}
+};
+
+
+
