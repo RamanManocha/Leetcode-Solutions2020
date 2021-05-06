5c8f08d3797518d5da5690ad2e21a3d0d99342b5
ramankgp <39129293+ramankgp@users.noreply.github.com>
2021-05-06 16:16:00 +0530

    Update CDC Maximum Depth of Binary Tree.cpp

--- Trees/Standard Tree/Depth and Height Variety/CDC Maximum Depth of Binary Tree.cpp
+++ Trees/Standard Tree/Depth and Height Variety/CDC Maximum Depth of Binary Tree.cpp
@@ -51,3 +51,22 @@ public:
         return root ? 1 + max(maxDepth(root->left), maxDepth(root->right)) : 0;
     }
 };
+
+
+
+more run time
+
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root)
+            return 0;
+        if(!root->left && !root->right)
+            return 1;
+        if(!root->left)
+            return maxDepth(root->right)+1;
+        if(!root->right)
+            return maxDepth(root->left)+1;
+        return max(maxDepth(root->left),maxDepth(root->right))+1;    }
+};
